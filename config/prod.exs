806c665f619a48f7b2fd154c420a1f6c0bb257a6
manuelgeek <emashmagak@appslab.co.ke>
2020-04-13 16:58:41 +0300

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :toast, ToastWeb.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :toast, ToastWeb.Repo,
+config :toast, Toast.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
